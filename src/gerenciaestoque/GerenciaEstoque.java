e66845e1a00a5453f4824a53e9e8e695680bf5d6
leandrospan <leandrospan@gmai.com>
2023-10-01 16:59:17 -0300

    Primeira subida

---
+++ src/gerenciaestoque/GerenciaEstoque.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gerenciaestoque;
+
+/**
+ *
+ * @author Leandro
+ */
+public class GerenciaEstoque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
